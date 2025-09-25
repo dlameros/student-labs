a523132a281920dff09a6879dfc2deb508ee5fb2
Dlameros <145215615+dlameros@users.noreply.github.com>
2025-09-25 14:35:16 +0300

    Delete LR3 directory

--- LR3/spider/scrapy-tutorial-starter/my_first_spider/my_first_spider/__init__.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-start_urls = ['https://quotes.toscrape.com']
-def parse(self, response):
-    self.logger.info('Hi, its my spider')
-    pass
